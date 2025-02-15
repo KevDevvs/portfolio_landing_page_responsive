7112df1547257fd80aff544d788d62a080e583a7
Keven <keven.alves@hotmail.com>
2025-02-15 18:38:39 -0300

    [fix] Active mobile btn fixed

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('#mobile_btn').on('click', function() {
-        $('mobile_menu').toggleClass('active')
+        $('#mobile_menu').toggleClass('active')
     });
 });
